297a8c7b15e1c55f5dc77b16ea5df333c15fc41b
Hamda Abdullah <hamdaabdullah@hamdas-macbook-pro.local>
2018-01-26 20:58:06 +0300

    Initial Commit

---
+++ TaskOneSecondTRy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskOneSecondTRy
+//
+//  Created by Hamda Abdullah  on 09/05/1439 AH.
+//  Copyright © 1439 Hamda Abdullah . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
